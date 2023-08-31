8cf4f958a557fe4283a1a210375dfe61640a1c68
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2023-08-31 16:47:32 +0500

    Add price pop-up

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,68 +1,95 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Link from "next/link";
+import { useState } from "react";
 import { Check } from "lucide-react";
+import { XCircle } from "lucide-react";
 export default function Pricing() {
+  const [isDivVisible, setIsDivVisible] = useState(true);
+  function toggleDivVisibility() {
+    setIsDivVisible(!isDivVisible);
+  }
   return (
     <>
       <div className="flex w-full flex-col  bg-[#EFF8FF]">
         <Header />
-        <div className="flex justify-end  py-2 pr-4 font-normal text-btnsecondary">
+        <div className="flex  justify-end  py-2 pr-4 font-normal text-btnsecondary">
           <Link href="/">Home</Link>/ Pricing
         </div>
-        <div className="flex w-full flex-col gap-3  p-10 pl-10">
-          <span className=" text-6xl font-bold text-primary">
+
+        <div className="flex h-[40px] justify-end ">
+          {!isDivVisible && (
+            <button
+              onClick={toggleDivVisibility}
+              className="field-drop-shadow mr-4 w-[160px] rounded-lg bg-btnprimary p-2 text-white transition-transform hover:scale-105"
+            >
+              About Pricing
+            </button>
+          )}
+        </div>
+
+        <div className="flex w-full flex-col gap-3  p-10 pl-10 max-sm:text-center">
+          <span className=" text-6xl font-bold text-primary max-md:text-5xl">
             Join our community <br /> Grow yours
           </span>
-          <span className="py-6 text-3xl font-normal text-[#757575]">
+          <span className="py-6 text-3xl font-normal text-[#757575] max-md:text-2xl">
             Reach 90 million eventgoers on <br /> the world's largest event{" "}
             <br />
             marketplace.
           </span>
-          <div className="div-drop-shadow absolute right-[68px] top-[311px] flex  flex-col rounded-lg  bg-white p-5">
-            <div className=" flex flex-col  p-10">
-              <span className="text-4xl font-bold text-[#313131]">
-                About Pricing
-              </span>
-              <ul className="flex flex-col gap-2 py-7 text-2xl text-[#757575]">
-                <li className="flex items-center  gap-3">
-                  <Check className="text-[#FF0000]" />
-                  Flexible ticket types for in-person or online events
-                </li>
-                <li className="flex items-center  gap-3">
-                  <Check className="text-[#FF0000]" />
-                  Unlimited listings in our marketplace
-                </li>
-                <li className="flex items-center  gap-3">
-                  <Check className="text-[#FF0000]" />
-                  Integrated email and social media marketing tools
-                </li>
-                <li className="flex items-center  gap-3">
-                  <Check className="text-[#FF0000]" />
-                  Ability to sell tickets on your own site
-                </li>
-                <li className="flex items-center  gap-3">
-                  <Check className="text-[#FF0000]" />
-                  Seamless attendee checkout experience
-                </li>
-                <li className="flex items-center gap-3">
-                  <Check className="text-[#FF0000]" />
-                  Payouts before your event happens
-                </li>
-                <li className="flex items-center  gap-3">
-                  <Check className="text-[#FF0000]" />
-                  Detailed analytics and reporting
-                </li>
-              </ul>
+
+          {isDivVisible && (
+            <div className="div-drop-shadow absolute right-[68px] top-[311px] flex flex-col rounded-lg bg-white p-5   max-sm:right-0 ">
+              <button
+                onClick={toggleDivVisibility}
+                className="absolute right-2 top-2 cursor-pointer p-2"
+              >
+                <XCircle className="text-btnsecondary" size={25} />
+              </button>
+              <div className=" flex flex-col  p-10 max-md:p-4">
+                <span className="text-4xl font-bold text-[#313131] max-md:text-3xl">
+                  About Pricing
+                </span>
+                <ul className="flex flex-col gap-2 py-7 text-2xl text-[#757575] max-md:text-xl max-sm:text-sm">
+                  <li className="flex items-center  gap-3 ">
+                    <Check className="text-[#FF0000]" />
+                    Flexible ticket types for in-person or online events
+                  </li>
+                  <li className="flex items-center  gap-3">
+                    <Check className="text-[#FF0000]" />
+                    Unlimited listings in our marketplace
+                  </li>
+                  <li className="flex items-center  gap-3">
+                    <Check className="text-[#FF0000]" />
+                    Integrated email and social media marketing tools
+                  </li>
+                  <li className="flex items-center  gap-3">
+                    <Check className="text-[#FF0000]" />
+                    Ability to sell tickets on your own site
+                  </li>
+                  <li className="flex items-center  gap-3">
+                    <Check className="text-[#FF0000]" />
+                    Seamless attendee checkout experience
+                  </li>
+                  <li className="flex items-center gap-3">
+                    <Check className="text-[#FF0000]" />
+                    Payouts before your event happens
+                  </li>
+                  <li className="flex items-center  gap-3">
+                    <Check className="text-[#FF0000]" />
+                    Detailed analytics and reporting
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
+          )}
         </div>
-        <div className=" flex w-full flex-col gap-4 bg-white  p-10 pl-10">
-          <span className="text-5xl font-bold text-primary">
+        <div className=" flex w-full flex-col gap-4 bg-white  p-10 pl-10 max-sm:items-center max-sm:justify-center max-sm:text-center">
+          <span className="text-5xl font-bold text-primary max-md:text-4xl">
             Simple pricing, <br />
             clear value
           </span>
-          <span className="text-2xl font-normal text-[#757575]">
+          <span className="text-2xl font-normal text-[#757575] max-md:text-xl">
             Lorem ipsum dolor sit amet,
             <br /> consectetur adipiscing elit.{" "}
           </span>
@@ -78,7 +105,7 @@ export default function Pricing() {
           }}
         >
           <div className="flex flex-col">
-            <span className="py-10 text-center text-7xl font-bold text-primary">
+            <span className="py-10 text-center text-7xl font-bold text-primary max-sm:text-6xl">
               FAQ's
             </span>
             <div className="flex w-full flex-col p-5 md:flex-row md:p-10">
