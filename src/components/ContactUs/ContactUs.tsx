893c3ceda34ed871925c800dd6147bc9b26620c6
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2023-08-31 15:19:42 +0500

    Changed Font Sizes

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -1,7 +1,7 @@
 export default function ContactUs() {
   return (
     <div className="h-50 flex w-full flex-col items-center justify-center gap-5 bg-[#F2F9FF] p-10">
-      <span className="mt-3 text-center text-5xl font-bold text-primary">
+      <span className="mt-3 text-center text-5xl font-bold text-primary max-sm:text-4xl">
         Still have questions?
       </span>
       <button className="mb-4 mt-4 w-[230px] rounded-lg bg-btnprimary p-2 text-white hover:bg-btnsecondary">
--- src/pages/help-center.tsx
+++ src/pages/help-center.tsx
@@ -21,13 +21,13 @@ export default function HelpCenter() {
   });
   return (
     <>
-      <div className="flex w-full flex-col  bg-[#EFF8FF]">
+      <div className="flex w-full flex-col  bg-[#EFF8FF] ">
         <Header />
         <div className="flex justify-end  py-2 pr-4 font-normal text-btnsecondary">
           <Link href="/help-center">Help Center</Link>
         </div>
         <div className="flex w-full flex-col gap-3  p-10">
-          <span className=" text-center text-6xl font-bold text-primary">
+          <span className=" text-center text-6xl font-bold text-primary max-sm:text-5xl">
             How can we help?
           </span>
           <form
@@ -48,17 +48,17 @@ export default function HelpCenter() {
         </div>
       </div>
       <div className="top[-22px] relative mx-auto flex  justify-center max-sm:flex-col ">
-        <div className="flex max-w-[640px] flex-col   ">
-          <div className=" bg-primary p-2 text-center text-xl font-normal text-white">
+        <div className="flex max-w-[640px] flex-col">
+          <div className="bg-primary p-2 text-center text-xl font-normal text-white">
             Attending an event
           </div>
-          <div className="flex w-full flex-wrap justify-between  gap-8  p-10 max-lg:justify-center">
-            <button className="field-drop-shadow flex items-center gap-4 rounded-lg p-3 px-5 text-xl">
-              <StaticImage src={FileSearch} alt="Logo" className=" h-6 w-6" />
+          <div className="flex w-full flex-wrap justify-between gap-8 p-10 max-lg:justify-center">
+            <button className="field-drop-shadow flex items-center gap-4 rounded-lg p-3 px-5 text-xl transition-transform hover:scale-105">
+              <StaticImage src={FileSearch} alt="Logo" className="h-6 w-6" />
               Find your tickets
             </button>
-            <button className="field-drop-shadow flex items-center gap-4 rounded-lg p-3 px-5 text-xl">
-              <StaticImage src={ContactBook} alt="Logo" className=" h-6 w-6" />
+            <button className="field-drop-shadow flex items-center gap-4 rounded-lg p-3 px-5 text-xl transition-transform hover:scale-105">
+              <StaticImage src={ContactBook} alt="Logo" className="h-6 w-6" />
               Contact organizer
             </button>
           </div>
@@ -68,11 +68,11 @@ export default function HelpCenter() {
             Organizing an event
           </div>
           <div className="flex w-full flex-wrap justify-between gap-8  p-10 max-lg:justify-center">
-            <button className="field-drop-shadow flex items-center gap-4 rounded-lg p-3 px-5 text-xl">
+            <button className="field-drop-shadow flex items-center gap-4 rounded-lg p-3 px-5 text-xl transition-transform hover:scale-105">
               <StaticImage src={FileTransfer} alt="Logo" className=" h-6 w-6" />
               Transfer tickets
             </button>
-            <button className="field-drop-shadow flex items-center gap-4 rounded-lg p-3 px-5 text-xl">
+            <button className="field-drop-shadow flex items-center gap-4 rounded-lg p-3 px-5 text-xl transition-transform hover:scale-105">
               <StaticImage src={EditInfo} alt="Logo" className=" h-6 w-6" />
               Edit order info
             </button>
@@ -80,13 +80,13 @@ export default function HelpCenter() {
         </div>
       </div>
 
-      <div className="container">
-        <span className="text-4xl font-bold text-primary">
+      <div className="container mt-5">
+        <span className=" pl-10 text-4xl font-bold text-primary">
           Browse by topics
         </span>
       </div>
 
-      <div className="mt-10 flex flex-wrap justify-between gap-10 p-10 max-md:justify-center">
+      <div className="mt-10 flex flex-wrap justify-between gap-10 p-10 max-md:justify-center 2xl:justify-evenly">
         <button className="field-drop-shadow relative flex w-[300px] flex-col items-center justify-center overflow-visible rounded-lg p-5 text-xl">
           <div className="absolute left-1/2 top-[-26px] -translate-x-1/2 transform rounded-full bg-[#DEF0FF] p-3">
             <StaticImage
