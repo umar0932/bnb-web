aef83b4840f72f3328e304241f7ab65e297e72b9
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2023-09-05 13:28:03 +0500

    Add AutoComplete Component

--- src/env.mjs
+++ src/env.mjs
@@ -16,6 +16,7 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
+    NEXT_PUBLIC_REACT_APP_MAP_KEY: z.string(),
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
   },
 
@@ -24,6 +25,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
+    NEXT_PUBLIC_REACT_APP_MAP_KEY: process.env.NEXT_PUBLIC_REACT_APP_MAP_KEY,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
--- src/pages/location.tsx
+++ src/pages/location.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header/Header";
 import { Progress } from "@/components/ui/progress";
+import Autocomplete from "@/components/AutoComplete/AutoCompleteAcc";
 import CurrentLocation from "../../public/assets/current_location.svg";
 import BrowserLocations from "../../public/assets/browse_online.svg";
 import Link from "next/link";
@@ -13,18 +14,21 @@ export default function Location() {
         <div className="flex h-full flex-col lg:flex-row">
           <div className="flex w-full flex-col bg-[#EFF8FF]  p-0 max-lg:order-2 sm:p-10 lg:w-1/2">
             <div className="m-5 bg-white p-5 sm:p-10 ">
-              <div className="flex justify-between">
+              <div className="flex flex-col">
                 <span className="text-3xl text-btnprimary">
                   Looking for events in
                 </span>
+                <Autocomplete />
               </div>
-              <div className="mt-3 flex   p-2">
+
+              {/* <div className="mt-3 flex   p-2">
+                <Autocomplete />
                 <input
                   className="w-full border-b border-[#757575]  p-2 text-3xl font-bold text-primary outline-none placeholder:text-primary"
                   placeholder="California"
                 />
-              </div>
-              <div className="mt-2 flex flex-col bg-[#F6FBFF] p-5">
+              </div> */}
+              {/* <div className="mt-2 flex flex-col bg-[#F6FBFF] p-5">
                 <div className=" flex gap-5 border-b border-[#9CB2DF] p-2">
                   <StaticImage src={CurrentLocation} alt="current-location" />
                   <span className="text-[#3B5998]">
@@ -35,7 +39,7 @@ export default function Location() {
                   <StaticImage src={BrowserLocations} alt="browse-location" />
                   <span className="text-[#3B5998]">Browse online events</span>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="flex w-full flex-col justify-center p-10 max-lg:order-1 lg:ml-10 lg:w-1/2">
