21acd1637de25e91a5a22d901edaa779cf4fbd07
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2023-09-13 12:29:13 +0500

    Date&Time Form Added

--- src/pages/create-event-page/create-event.tsx
+++ src/pages/create-event-page/create-event.tsx
@@ -1,5 +1,7 @@
 import BasicInfoForm from "@/components/Forms/BasicInfoForm";
+import DateTimeForm from "@/components/Forms/DateTimeForm";
 import LocationForm from "@/components/Forms/LocationForm";
+import Link from "next/link";
 
 export default function CreateEvent() {
   return (
@@ -7,6 +9,19 @@ export default function CreateEvent() {
       <div className="flex flex-col bg-[#F1F9FF] p-10 max-sm:p-5">
         <BasicInfoForm />
         <LocationForm />
+        <DateTimeForm />
+        <div className="mt-10 flex w-full justify-end gap-10 max-sm:flex-col max-sm:items-center max-sm:gap-5">
+          <Link href="/create-event-page/first-step">
+            <button className="flex h-[43px] w-[170px] items-center justify-center rounded-sm bg-[#DEF0FF] p-4 font-bold text-primary max-sm:w-[160px]">
+              Discard
+            </button>
+          </Link>
+          <Link href="/create-event-page/third-step">
+            <button className="flex h-[43px] w-[170px] items-center justify-center rounded-sm bg-btnprimary p-4 font-bold text-white hover:bg-btnsecondary max-sm:w-[160px]">
+              Save & Continue
+            </button>
+          </Link>
+        </div>
       </div>
     </>
   );
