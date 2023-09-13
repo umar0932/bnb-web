8d22bb0d2721fa318ccf8f685c9d77811c60df71
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2023-09-13 11:11:46 +0500

    Location Form Added

--- src/pages/create-event-page/create-event.tsx
+++ src/pages/create-event-page/create-event.tsx
@@ -1,10 +1,12 @@
 import BasicInfoForm from "@/components/Forms/BasicInfoForm";
+import LocationForm from "@/components/Forms/LocationForm";
 
 export default function CreateEvent() {
   return (
     <>
       <div className="flex flex-col bg-[#F1F9FF] p-10 max-sm:p-5">
         <BasicInfoForm />
+        <LocationForm />
       </div>
     </>
   );
