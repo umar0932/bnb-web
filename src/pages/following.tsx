ac5c8513c3ae6c3b4a2d7177a0cee182a297137b
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2023-09-15 17:04:03 +0500

    Alignment Issues Fixed

--- src/pages/following.tsx
+++ src/pages/following.tsx
@@ -9,7 +9,7 @@ export default function Following() {
     <>
       <div className=" min-2xl:items-center flex h-[100vh] flex-col">
         <Header colorScheme="follow-page-header" />
-        <div className="mt-10 h-full bg-[#DEF0FF]">
+        <div className="mt-10 flex  h-full items-center justify-center bg-[#DEF0FF]">
           <div className="flex  justify-between gap-10 overflow-auto  p-10">
             {/* {eventsArray.map((event, index) => (
               <EventCard key={index} event={event} />
--- src/pages/likes.tsx
+++ src/pages/likes.tsx
@@ -9,7 +9,7 @@ export default function Likes() {
     <>
       <div className=" min-2xl:items-center flex h-[100vh] flex-col">
         <Header colorScheme="likes-page-header" />
-        <div className=" mt-10 h-full bg-[#DEF0FF]">
+        <div className=" mt-10 flex  h-full items-center justify-center bg-[#DEF0FF]">
           <div className=" flex gap-10 overflow-auto bg-[#DEF0FF] p-10">
             {eventsArray.map((event, index) => (
               <EventCard key={index} event={event} />
