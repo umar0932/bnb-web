4e7bc62a6eceda67fa691e66d0d82ec830d243e5
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2023-09-18 15:56:08 +0500

    ContactInfo Form Added

---
+++ src/components/Forms/AccountSettingsForm/ContactInfoForm.tsx
@@ -0,0 +1,430 @@
+import { useFormik } from "formik";
+import { useState } from "react";
+import { Checkbox } from "@/core/ui/checkbox";
+import { Button } from "@/core/ui/button";
+import PlusIcon from "../../../../public/assets/plus_icon.svg";
+import Image from "next/image";
+import { Input } from "@/core/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/core/ui/select";
+export default function ContactInfoForm() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const formik = useFormik({
+    initialValues: {
+      first_name: "",
+      last_name: "",
+      home_phone: "",
+      cell_phone: "",
+      job_title: "",
+      company_name: "",
+      website: "",
+      address: "",
+      address_two: "",
+      city: "",
+      postal_code: "",
+      work_address: "",
+      work_address_two: "",
+      work_city: "",
+      work_postal_code: "",
+    },
+    onSubmit: (values) => {
+      JSON.stringify(values, null, 2);
+    },
+  });
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0]; // Get the first selected file
+    setSelectedFile(file);
+  };
+  return (
+    <>
+      <div className="card-drop-shadow flex gap-10 rounded-lg bg-white p-10 max-sm:p-5">
+        <div className="flex w-full flex-col">
+          <span className="text-4xl font-bold text-primary max-sm:text-3xl">
+            Account Information
+          </span>
+          <form
+            onSubmit={formik.handleSubmit}
+            className="mt-2 flex w-full gap-5 max-lg:flex-col"
+          >
+            <div className="flex w-[80%] flex-col max-lg:w-full">
+              {/* First Name */}
+              <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                <span className="text-sm text-[#757575]">First Name</span>
+                <input
+                  className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                  id="first_name"
+                  name="first_name"
+                  type="text"
+                  placeholder="Enter your first name"
+                  onChange={formik.handleChange}
+                  value={formik.values.first_name}
+                />
+              </div>
+
+              {/* Last Name */}
+
+              <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                <span className="text-sm text-[#757575]">Last Name</span>
+                <input
+                  className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                  id="last_name"
+                  name="last_name"
+                  type="text"
+                  placeholder="Enter your last name"
+                  onChange={formik.handleChange}
+                  value={formik.values.last_name}
+                />
+              </div>
+
+              {/* Home Phone */}
+
+              <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                <span className="text-sm text-[#757575]">Home Phone</span>
+                <input
+                  className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                  id="home_phone"
+                  name="home_phone"
+                  type="number"
+                  placeholder="Enter your home phone"
+                  onChange={formik.handleChange}
+                  value={formik.values.home_phone}
+                />
+              </div>
+
+              {/* Cell Phone */}
+
+              <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                <span className="text-sm text-[#757575]">Cell Phone</span>
+                <input
+                  className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                  id="cell_phone"
+                  name="cell_phone"
+                  type="number"
+                  placeholder="Enter your cell phone"
+                  onChange={formik.handleChange}
+                  value={formik.values.cell_phone}
+                />
+              </div>
+
+              {/* Job Title */}
+
+              <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                <span className="text-sm text-[#757575]">Cell Phone</span>
+                <input
+                  className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                  id="job_title"
+                  name="job_title"
+                  type="text"
+                  placeholder="Enter your job title"
+                  onChange={formik.handleChange}
+                  value={formik.values.job_title}
+                />
+              </div>
+
+              {/* Company/Organization Name */}
+
+              <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                <span className="text-sm text-[#757575]">
+                  Company/ Organization
+                </span>
+                <input
+                  className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                  id="company_name"
+                  name="company_name"
+                  type="text"
+                  placeholder="Enter your company name"
+                  onChange={formik.handleChange}
+                  value={formik.values.company_name}
+                />
+              </div>
+
+              {/*Website Name*/}
+
+              <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                <span className="text-sm text-[#757575]">Website</span>
+                <input
+                  className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                  id="website_name"
+                  name="website_name"
+                  type="url"
+                  placeholder="Enter website address"
+                  onChange={formik.handleChange}
+                  value={formik.values.website}
+                />
+              </div>
+
+              <div className="mt-20">
+                <span className="text-4xl font-bold text-primary max-sm:text-3xl">
+                  Home Address
+                </span>
+                {/* Address */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">Address</span>
+                  <input
+                    className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                    id="address"
+                    name="address"
+                    type="text"
+                    placeholder="Enter your address"
+                    onChange={formik.handleChange}
+                    value={formik.values.address}
+                  />
+                </div>
+
+                {/* Second Address */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">Address 2</span>
+                  <input
+                    className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                    id="address_two"
+                    name="address_two"
+                    type="text"
+                    placeholder="Enter your second address"
+                    onChange={formik.handleChange}
+                    value={formik.values.address_two}
+                  />
+                </div>
+
+                {/* City */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">City</span>
+                  <input
+                    className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                    id="city"
+                    name="city"
+                    type="text"
+                    placeholder="Enter your city name"
+                    onChange={formik.handleChange}
+                    value={formik.values.city}
+                  />
+                </div>
+
+                {/* Country */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">Country</span>
+                  <Select>
+                    <SelectTrigger className="w-full border-none bg-[#EFF8FF] text-[#757575] ">
+                      <SelectValue placeholder="United States" />
+                    </SelectTrigger>
+                    <SelectContent className="text-[#313131]">
+                      <SelectGroup>
+                        <SelectItem value="United States">
+                          United States
+                        </SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                {/*Zip/Postal Code*/}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">
+                    Zip/ Postal Code
+                  </span>
+                  <input
+                    className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                    id="postal_code"
+                    name="postal_code"
+                    type="number"
+                    placeholder="Enter your Zip Code"
+                    onChange={formik.handleChange}
+                    value={formik.values.postal_code}
+                  />
+                </div>
+
+                {/* State */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">State</span>
+                  <Select>
+                    <SelectTrigger className="w-full border-none bg-[#EFF8FF] text-[#757575] ">
+                      <SelectValue placeholder="California" />
+                    </SelectTrigger>
+                    <SelectContent className="text-[#313131]">
+                      <SelectGroup>
+                        <SelectItem value="United States">
+                          California
+                        </SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+
+              {/* Work Address */}
+              <div className="mt-20">
+                <span className="text-4xl font-bold text-primary max-sm:text-3xl">
+                  Work Address
+                </span>
+                {/* Address */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">Address</span>
+                  <input
+                    className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                    id="address"
+                    name="address"
+                    type="text"
+                    placeholder="Enter your address"
+                    onChange={formik.handleChange}
+                    value={formik.values.work_address}
+                  />
+                </div>
+
+                {/* Second Address */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">Address 2</span>
+                  <input
+                    className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                    id="address_two"
+                    name="address_two"
+                    type="text"
+                    placeholder="Enter your second address"
+                    onChange={formik.handleChange}
+                    value={formik.values.work_address_two}
+                  />
+                </div>
+
+                {/* City */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">City</span>
+                  <input
+                    className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                    id="city"
+                    name="city"
+                    type="text"
+                    placeholder="Enter your city name"
+                    onChange={formik.handleChange}
+                    value={formik.values.work_city}
+                  />
+                </div>
+
+                {/* Country */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">Country</span>
+                  <Select>
+                    <SelectTrigger className="w-full border-none bg-[#EFF8FF] text-[#757575] ">
+                      <SelectValue placeholder="United States" />
+                    </SelectTrigger>
+                    <SelectContent className="text-[#313131]">
+                      <SelectGroup>
+                        <SelectItem value="United States">
+                          United States
+                        </SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                {/*Zip/Postal Code*/}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">
+                    Zip/ Postal Code
+                  </span>
+                  <input
+                    className="h-[25px] bg-transparent outline-none placeholder:text-sm"
+                    id="postal_code"
+                    name="postal_code"
+                    type="number"
+                    placeholder="Enter your Zip Code"
+                    onChange={formik.handleChange}
+                    value={formik.values.work_postal_code}
+                  />
+                </div>
+
+                {/* State */}
+                <div className="mt-5 flex  flex-col rounded-lg bg-[#EFF8FF] p-3">
+                  <span className="text-sm text-[#757575]">State</span>
+                  <Select>
+                    <SelectTrigger className="w-full border-none bg-[#EFF8FF] text-[#757575] ">
+                      <SelectValue placeholder="California" />
+                    </SelectTrigger>
+                    <SelectContent className="text-[#313131]">
+                      <SelectGroup>
+                        <SelectItem value="United States">
+                          California
+                        </SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 flex h-[190px] w-[190px] flex-col items-center justify-center rounded-lg bg-[#EFF8FF] p-5">
+              <label
+                htmlFor="picture"
+                className="flex h-[80px] w-[80px] flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-white"
+              >
+                <Image
+                  src={PlusIcon}
+                  alt="plus_icon"
+                  className="cursor-pointer"
+                />
+                <input
+                  type="file"
+                  id="picture"
+                  accept="image/*"
+                  style={{ display: "none" }}
+                  onChange={handleFileChange}
+                />
+              </label>
+              <span className="mt-5 text-center text-sm">
+                Upload Profile Picture
+              </span>
+              {selectedFile ? (
+                <p>Selected file: {(selectedFile as File).name}</p>
+              ) : null}
+            </div>
+          </form>
+
+          {/* Billing Address */}
+          <div className="mt-10 flex flex-col gap-2">
+            <span className="text-4xl font-bold text-primary max-sm:text-3xl">
+              Billing Address
+            </span>
+            <div className="flex items-center space-x-2">
+              <Checkbox className="text-btnseondary" />
+              <span className="text-[#757575]">
+                Same as home address information
+              </span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Checkbox />
+              <span className="text-[#757575]">
+                Same as work address information
+              </span>
+            </div>
+          </div>
+
+          {/* Shipping Address */}
+          <div className="mt-10 flex flex-col gap-2">
+            <span className="text-4xl font-bold text-primary max-sm:text-3xl">
+              Shipping Address
+            </span>
+            <div className="flex items-center space-x-2">
+              <Checkbox className="text-btnseondary" />
+              <span className="text-[#757575]">
+                Same as home address information
+              </span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Checkbox />
+              <span className="text-[#757575]">
+                Same as work address information
+              </span>
+            </div>
+          </div>
+          <Button className="mt-10 w-[130px] bg-btnprimary font-bold text-white hover:bg-btnsecondary">
+            Save
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/account-settings.tsx
+++ src/pages/account-settings.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import AuthHeader from "@/components/Header/AuthHeader";
+import ContactInfoForm from "@/components/Forms/AccountSettingsForm/ContactInfoForm";
 
 export default function AccountSetting() {
-  const [selectedDiv, setSelectedDiv] = useState(null);
+  const [selectedDiv, setSelectedDiv] = useState(0); // Set the initial state to 0 (index of "Contact Information")
 
   const handleDivClick = (index: any) => {
     setSelectedDiv(index);
@@ -12,9 +13,13 @@ export default function AccountSetting() {
     <>
       <div className="flex flex-col bg-[#DEF0FF]">
         <AuthHeader />
-        <div className="flex">
-          <div className=" w-[60%] border border-red-500"></div>
-          <div className="mt-10 flex w-[40%] flex-wrap items-center justify-center gap-5 border border-green-500 p-5">
+        <div className="flex max-md:flex-col">
+          <div className=" relative w-[60%]  max-md:order-2 max-md:w-full">
+            <div className=" max-md:full absolute w-full p-10 max-md:top-full max-md:order-3 max-md:w-full max-sm:p-5">
+              <ContactInfoForm />
+            </div>
+          </div>
+          <div className="mt-10 flex w-[40%] flex-wrap items-center justify-center gap-5  p-5 max-md:order-1 max-md:w-full">
             {[
               "Contact Information",
               "Change Email",
